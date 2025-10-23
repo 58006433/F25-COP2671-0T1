5f6a5267aa3f9817359820772f91415067859b23
N.M.P. <nat.m.portant@gmail.com>
2025-10-22 22:27:44 -0400

    add map

--- Projects/Final Project/MyFinalProject/Assets/Scripts/PlayerController.cs
+++ Projects/Final Project/MyFinalProject/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour
 {
+    public float movementSpeed = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(movementSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-movementSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, movementSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -movementSpeed * Time.deltaTime, 0);
+        }
     }
 }
